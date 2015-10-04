10bfe334645fda39ba156953ed205776e9c36dba
Matthew Lavine <matt@matthewlavine.net>
2015-10-03 22:04:40 -0400

    Added winston Papertrail

--- utilities/logger.js
+++ utilities/logger.js
@@ -1,3 +1,13 @@
 var winston = require('winston');
 
+var Papertrail = require('winston-papertrail').Papertrail;
+
+winston.add(
+    new Papertrail({
+        host: process.env.PAPERTRAIL_URL,
+        port: provess.env.PAPERTRAIL_PORT,
+        level: 'error'
+    })
+);
+
 module.exports = winston;
